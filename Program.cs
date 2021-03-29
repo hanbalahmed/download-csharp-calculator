0ce192bb3d18261521d52e53035d6bc29623d4f7
Hanbal Ahmed Codes <72215552+hanbalahmed@users.noreply.github.com>
2021-03-29 19:12:30 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MyFirstCsProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Operator: ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter a number: ");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.WriteLine(number1 + number2);
+            } else if (op == "-")
+            {
+                Console.WriteLine(number1 - number2);
+            } else if (op == "*")
+            {
+                Console.WriteLine(number1 * number2);
+            } else if (op == "/")
+            {
+                Console.WriteLine(number1 / number2);
+            } else
+            {
+                Console.WriteLine("You have entered an invalid operator.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
